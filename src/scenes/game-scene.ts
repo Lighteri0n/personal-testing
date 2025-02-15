49eaed942e7b6bf37074e6d7047ffe84b94b002f
jorgeCalle2212 <jorge.calle2212@gmail.com>
2025-02-15 12:14:28 +0100

    trying spam-clicking for fish reeling

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,6 +1,7 @@
 import { Application, Container, Graphics, Text } from "pixi.js";
 import { createButton } from "../utils";
 import { setupShopScene } from "./shop-scene";
+import { ButtonContainer } from "@pixi/ui";
 
 /*
 *   Game Scene
@@ -16,68 +17,70 @@ export const setupGameScene = (params: {
 
   const gameBackground = new Container(); //TODO add background image
 
-  //fishing spot object test. The circle appears out of the player's LoS (the screen)
-  //and it'll start appearing once the player gets close to the circle's position.
-  //Added as a child of gameBackground container, so it will move along with it
-  //For now, as an unwanted behaviour, it might be loaded and drawn at all times
-  const fishingSpot = new Graphics();
-  fishingSpot.circle(0, 0, 50);
-  fishingSpot.fill(0x000000);
-  fishingSpot.x = 900;
-  fishingSpot.y = 700;
-  gameBackground.addChild(fishingSpot);
-  container.addChild(gameBackground);
-  const positionText = new Text({
-    text: 'Pos - X: 0, Y: 0',
-    style: {
-      fontFamily: "Arial",
-      fontSize: 20,
-      fill: 0xffffff,
-      align: "left",
-    },
-  });
-  positionText.anchor.set(0.5);
-  positionText.x = 25;
-  positionText.y = 25;
-  container.addChild(positionText);
-
 
   const kroniiBody = new Graphics();
   kroniiBody.rect(0, 0, 80, 160);
   kroniiBody.fill(0x0000ff);
   kroniiBody.x = (app.screen.width / 2) - kroniiBody.height / 2;
-  kroniiBody.y = (app.screen.height / 2) - kroniiBody.width / 2;
+  kroniiBody.y = (app.screen.height / 2) - 200;
   container.addChild(kroniiBody);
-  //text that appears once the player is inside a fishing spot bounds
-  const fishingText = new Text({
-    text: 'Fish!',
-    style: {
-      fontFamily: "Arial",
-      fontSize: 30,
-      fill: 0xffffff,
-      align: "center",
-    },
-  });
-  fishingText.anchor.set(0.5);
-  fishingText.x = kroniiBody.width / 2;
-  fishingText.y = -10;
-  fishingText.visible = false;
-  kroniiBody.addChild(fishingText);
+
+  const backpackButton = new ButtonContainer()
+  backpackButton
+  const backpack = new Graphics()
+  backpack.rect(0, 0, 60, 60);
+  backpack.fill(0x5c3e0e);
+  backpack.x = (app.screen.width / 2) + 100;
+  backpack.y = (app.screen.height / 2) - 100;
+  backpackButton.onPress.connect(()=> {
+      inventoryContainer.visible=!inventoryContainer.visible;});
+  backpackButton.addChild(backpack);
+  container.addChild(backpackButton);
 
   //Container for the inventroy HUD
   const inventoryContainer = new Container();
-  inventoryContainer.x = app.screen.width;
-  inventoryContainer.y = 0;
+  inventoryContainer.visible = false;
+  inventoryContainer.x = app.screen.width-(app.screen.width * 0.4);
+  inventoryContainer.y = 10;
   const inventoryBackground = new Graphics();
-  inventoryBackground.rect(0, 0, app.screen.width * 0.15, app.screen.height);
+  inventoryBackground.rect(0, 0, app.screen.width * 0.40, app.screen.height *0.95);
   inventoryBackground.fill(0x0A0AFF);
   inventoryContainer.addChild(inventoryBackground);
   container.addChild(inventoryContainer);
 
-  let isInventoryVisible = false; //these three are for the animation
-  let startTime = 0; //of the inventory menu
-  let animationCooldown = false; //
 
+  //test for the fishing mechanic
+  const fishingStatusContainer = new Container();
+  fishingStatusContainer.x = 200;
+  fishingStatusContainer.y = 200;
+  const fishingText = new Text({
+    text: "Press Q to start fishing",
+    style: {
+      fontFamily: "Arial",
+      fontSize: 24,
+      fill: 0xffffff,
+    },
+  });
+    fishingText.anchor.set(0.5);
+    fishingText.x = 10;
+    fishingText.y = 30;
+  fishingStatusContainer.addChild(fishingText);
+  const playerFishingBar = new Graphics();
+  playerFishingBar.rect(0, 0, 40, 1);
+  playerFishingBar.fill(0x00FF00);
+  playerFishingBar.angle = 180
+  fishingStatusContainer.addChild(playerFishingBar);
+  
+  const fishScapeBar = new Graphics();
+  fishScapeBar.rect(50, 0, 40, 1);
+  fishScapeBar.fill (0xFF0000);
+  fishScapeBar.angle = 180;
+  fishingStatusContainer.addChild(fishScapeBar);
+  container.addChild(fishingStatusContainer);
+  let isPlayerFishing = false;
+  let elapsedTime = 0;
+  let playerProgress = 0;
+  let fishProgress = 0;
   /*
   *   GAME LOOP
   *
@@ -87,10 +90,29 @@ export const setupGameScene = (params: {
     if (container.visible) {
       movePlayer();
     }
-
-    fishingText.visible = fishingActionAvailable();
-
-    positionText.text = `Pos - X: ${kroniiBody.x}, Y: ${kroniiBody.y}`;
+    if(isPlayerFishing){
+      elapsedTime+= app.ticker.deltaMS / 1000;
+      fishingText.text = fishProgress+"% - "+playerProgress+"%";
+      if(elapsedTime>=0.25){
+        fishScapeBar.height+=5;
+        playerProgress = playerFishingBar.height;
+        fishProgress = fishScapeBar.height;
+        elapsedTime=0;
+        if((playerProgress>=100)||(fishProgress>=100)){
+          isPlayerFishing=false;
+          if(playerProgress>=100){
+            fishingText.text="Player wins. Press Q to restart"
+          }else{
+            fishingText.text= "Fish scaped. Press Q to restart";
+          }
+          fishScapeBar.height= 5;
+          playerProgress = 0;
+          fishProgress = 0;
+          playerFishingBar.height = 5;
+        };
+      };
+    };
+    
   });
 
   const keys: Record<string, boolean> = {};
@@ -100,58 +122,29 @@ export const setupGameScene = (params: {
   window.addEventListener("keyup", (e) => {
     keys[e.key] = false;
   });
+  window.addEventListener("pointerdown", (e)=>{
+    if(isPlayerFishing){
+      playerFishingBar.height+=5;
+    };
+  });
 
   // TODO Rename to playerControls or set the E key to another function
   const movePlayer = () => {
-    if (keys["W"] || keys["w"]) gameBackground.y += 5;
-    if (keys["D"] || keys["d"]) gameBackground.x -= 5;
-    if (keys["A"] || keys["a"]) gameBackground.x += 5;
-    if (keys["S"] || keys["s"]) gameBackground.y -= 5;
-
-    if ((keys["E"] || keys["e"]) && !animationCooldown) {
-      isInventoryVisible = !isInventoryVisible;
-      startTime = Date.now();
-      app.ticker.add(animateInventory);
-      animationCooldown = true;
-      setTimeout(() => {
-        animationCooldown = false;
-      }, 1000);
+    if (keys["D"] || keys["d"]){
+      kroniiBody.x += 7;
+      backpack.x += 7;
     }
-  };
-
-  const fishingActionAvailable = () => {
-    //function to check if the player is inside a fishing spot bounds, to enable
-    //the fishing minigame
-    //TODO set all fishing spots of the map
-    //TODO add fishing spots (array?) as parameter
-    //Store coordinates of both the player and the fishing spot
-    const playerBounds = kroniiBody.getBounds();
-    const fishingSpotBounds = fishingSpot.getBounds();
-    //calculate distance from the player to the fishing spot
-    const distX = Math.abs(playerBounds.x + playerBounds.width / 2 - fishingSpotBounds.x - fishingSpotBounds.width / 2);
-    const distY = Math.abs(playerBounds.y + playerBounds.height / 2 - fishingSpotBounds.y - fishingSpotBounds.height / 2);
-    //Pythagoras theorem
-    const distance = Math.sqrt(distX * distX + distY * distY);
-    //We simulate that the kroniiBody has a "radius" and set the distance in which
-    //both figures touch
-    const contactDistance = (Math.min(playerBounds.width, playerBounds.height) / 2) + fishingSpotBounds.width / 2;
-
-    return distance < contactDistance;
-  };
-
-  const animateInventory = () => {
-    // TODO Fix cooldown and hiding animation
-    const elapsedTime = (Date.now() - startTime) / 1000; //time in seconds
-    const targetX = isInventoryVisible ? app.screen.width * 0.85 : app.screen.width;
-
-    if (elapsedTime < 0.5) {
-      inventoryContainer.x = app.screen.width + (targetX - app.screen.width) * (elapsedTime / 0.5);
-    } else {
-      inventoryContainer.x = targetX;
-      app.ticker.remove(animateInventory);
-      animationCooldown = false;
+    if (keys["A"] || keys["a"]){
+      kroniiBody.x -= 7;
+      backpack.x -= 7;
     }
-  };
+    if ((keys["E"] || keys["e"])) {
+      inventoryContainer.visible=!inventoryContainer.visible;
+      };
+      if ((keys["Q"] || keys["q"])) {
+        isPlayerFishing=true;
+        };
+    };
 
   const shopSceneContainer = new Container();
   const fishDataButton = createButton({
